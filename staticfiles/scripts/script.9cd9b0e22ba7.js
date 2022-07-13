661fabd1c8b059e2e85093e3acf01f38d0a8b398
Deiker Diaz <deiker21diaz@gmail.com>
2022-07-13 14:19:53 -0400

    finished app deployed in heroku

---
+++ staticfiles/scripts/script.9cd9b0e22ba7.js
@@ -0,0 +1,36 @@
+var dropdown = document.getElementsByClassName("dropdown-icon");
+var auto_dropdown = document.getElementsByClassName("open");
+const navMenu = document.querySelector("#navMenu");
+
+navMenu.addEventListener( "click", () => {
+    navMenu.classList.toggle("active");
+    navMenu.nextElementSibling.classList.toggle("active");
+});
+
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.parentElement.classList.toggle("active");
+    this.previousElementSibling.classList.toggle("active");
+    this.classList.toggle("active");
+    var dropdownContent = this.parentElement.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+  });
+}
+
+
+var i;
+
+for (i = 0; i < auto_dropdown.length; i++) {
+    if (auto_dropdown[i].style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        auto_dropdown[i].style.display = "block";
+    }
+}
+
